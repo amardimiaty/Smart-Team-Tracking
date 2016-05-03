48e91af182005b91ff276a215977277a538a0ea9
Davide Meacci <davide.meacci@gmail.com>
2016-05-03 16:24:08 +0200

    funziona Jesus

--- SmartTeamTracking/src/main/java/com/pervasive/SmartTeamTrackingApplication.java
+++ SmartTeamTracking/src/main/java/com/pervasive/SmartTeamTrackingApplication.java
@@ -40,12 +40,12 @@ public class SmartTeamTrackingApplication {
 	
 	public static void testUser(GraphDatabaseService graphDatabaseService, UserRepository userRepository,BeaconRepository beaconRepository){
 	
-		User stefano = new User(1l,"Stefano","Conoci","stefano.conoci@gmail.com","badpw");
+		User stefano = new User(null,"Stefano","Conoci","stefano.conoci@gmail.com","badpw");
 		stefano.setLatGPS(20.0);
 		stefano.setLonGPS(40.2);
 		
-		//User davide = new User(new Long(2),"Davide","Meacci","davide.meacci@gmail.com","reallybadpw");
-		//Beacon b1 = new Beacon(1, "B2", 41.222, 45.23);
+		User davide = new User(null ,"Davide","Meacci","davide.meacci@gmail.com","reallybadpw");
+		Beacon b1 = new Beacon(null, "B2", 41.222d, 45.23d);
 		
 		//stefano.setInRange(b1);
 		//davide.setInRange(b1);
@@ -53,18 +53,24 @@ public class SmartTeamTrackingApplication {
 		Transaction tx = graphDatabaseService.beginTx();
 		try{
 			System.out.println("Creating random node");
-			graphDatabaseService.createNode();
+			//graphDatabaseService.createNode();
 			
-			//System.out.println("Saving users");
-			//userRepository.save(stefano);
-			//userRepository.save(davide);
+			System.out.println("Saving users");
+			userRepository.save(stefano);
+			userRepository.save(davide);
 			
-			//System.out.println("Saving beacon");
-			//beaconRepository.save(b1);
+			System.out.println("Saving beacon");
+			beaconRepository.save(b1);
 			
-		    //System.out.println("Retrieving user");
-			//User stefanoFromNeo = userRepository.findByName("Stefano");
-			//System.out.println(stefanoFromNeo.toString());
+		    System.out.println("Retrieving user");
+			User stefanoFromNeo = userRepository.findByName("Stefano");
+			System.out.println(stefanoFromNeo.toString());
+			User davideFromNeo = userRepository.findByName("Davide");
+			System.out.println(davideFromNeo.toString());
+			
+			System.out.println("Retrieving beacon");
+			Beacon beaconFromNeo = beaconRepository.findByName("B2");
+			System.out.println(beaconFromNeo.toString());
 			
 			tx.success();
 		}
