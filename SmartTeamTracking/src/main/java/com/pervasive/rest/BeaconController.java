01d340b08863652ad12063171be9670633ea99ab
biagio.botticelli <botticelli.1212666@studenti.uniroma1.it>
2016-05-27 20:59:25 +0200

    Modified deleteBeacon REST call

--- SmartTeamTracking/src/main/java/com/pervasive/rest/BeaconController.java
+++ SmartTeamTracking/src/main/java/com/pervasive/rest/BeaconController.java
@@ -83,7 +83,7 @@ public class BeaconController {
     
     
     @RequestMapping(method = RequestMethod.POST, value="/beacon/delete")
-    public String addBeacon(@RequestParam(value="id", required=true, defaultValue = "") String beaconID){
+    public String addBeacon(@RequestParam(value="id", defaultValue = "") String beaconID){
     	
     	BeaconRepository beaconRepository = (BeaconRepository) context.getBean(BeaconRepository.class);
         GraphDatabaseService graphDatabaseService = (GraphDatabaseService) context.getBean(GraphDatabaseService.class);
